faec5704b352a4cefa16dc461aa8a7804cab507a
WSL_SCD <49774633+Acid-Free@users.noreply.github.com>
2023-01-28 17:26:51 +0800

    Only select grid when mouse on hold

--- logic.js
+++ logic.js
@@ -2,27 +2,46 @@ let rowCount = 8;
 
 const dimensionsButton = document.querySelector(".dimensions-button");
 const grid = document.querySelector(".grid");
-// let gridItems = [];
+
+let canDraw = false;
+
+function listenToMouseHold() {
+  window.addEventListener("mousedown", (e) => (canDraw = true));
+  ["dragend", "mouseup"].forEach((event) =>
+    window.addEventListener(event, (e) => {
+      console.log("end");
+      canDraw = false;
+    })
+  );
+}
+
+function listenToDimensionsButton() {
+  dimensionsButton.addEventListener("click", (e) => {
+    console.log("pressed");
+  });
+}
 
 function createGridItems() {
   const itemCount = rowCount * rowCount;
   for (let i = 0; i < itemCount; ++i) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
-    gridItem.addEventListener("mouseenter", selectGridItem);
-    gridItem.addEventListener("mouseleave", deselectGridItem);
+    gridItem.addEventListener("mousemove", selectGridItem);
+    gridItem.setAttribute("draggable", false);
     grid.appendChild(gridItem);
   }
 }
 
 function selectGridItem(e) {
-  console.log("selected");
-  e.target.classList.add("grid-item-selected");
+  if (canDraw) e.target.classList.add("grid-item-selected");
 }
 
 function deselectGridItem(e) {
-  console.log("removed");
+  console.log("leave");
   e.target.classList.remove("grid-item-selected");
 }
 
+grid.setAttribute("draggable", false);
+listenToDimensionsButton();
+listenToMouseHold();
 createGridItems();
