46dff9bb3322d49ab79f15ed19210d6def44b29c
WSL_SCD <49774633+Acid-Free@users.noreply.github.com>
2023-01-28 17:43:33 +0800

    Add several features
    1. Dimensions button applies valid input to create new grid
    2. Dimensions button name reflects the current rowCount.

--- logic.js
+++ logic.js
@@ -9,7 +9,6 @@ function listenToMouseHold() {
   window.addEventListener("mousedown", (e) => (canDraw = true));
   ["dragend", "mouseup"].forEach((event) =>
     window.addEventListener(event, (e) => {
-      console.log("end");
       canDraw = false;
     })
   );
@@ -17,12 +16,32 @@ function listenToMouseHold() {
 
 function listenToDimensionsButton() {
   dimensionsButton.addEventListener("click", (e) => {
-    console.log("pressed");
+    let rowInput = prompt("Enter row count:");
+
+    if (!rowInput) {
+      alert("Empty input");
+      return;
+    } else if (isNaN(rowInput)) {
+      alert("Input is not a number");
+      return;
+    } else if (rowInput < 1 || rowInput > 100) {
+      alert("Input should be between 1 and 100");
+      return;
+    }
+
+    rowInput = parseInt(rowInput);
+
+    rowCount = rowInput;
+    removeGridItems();
+    createGridItems();
   });
 }
 
 function createGridItems() {
   const itemCount = rowCount * rowCount;
+
+  grid.style.gridTemplateColumns = `repeat(${rowCount}, 1fr)`;
+
   for (let i = 0; i < itemCount; ++i) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
@@ -30,6 +49,10 @@ function createGridItems() {
     gridItem.setAttribute("draggable", false);
     grid.appendChild(gridItem);
   }
+}
+
+function removeGridItems() {
+  grid.replaceChildren();
 }
 
 function selectGridItem(e) {
